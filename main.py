48786f7e426d1438c27baf544f908e6bc8af149e
jp066 <jpedro030506@gmail.com>
2024-09-06 21:38:20 -0300

    cirando classes cliente e pessoa fisica

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from random import randint
+from datetime import datetime
 
 menu = """
 --------------------------------
@@ -81,7 +82,7 @@ class Conta:
     def criar_conta(cls, cliente):
         numero = randint(1, 1000) # gera um número aleatório para a conta
         conta = cls(saldo=0.0, cliente=cliente) # cria um objeto da classe Conta
-        return conta
+        return conta # retorna o objeto criado, é chamado de factory method
     
     # esse método é um método de classe, pois ele não precisa de uma instância da classe para ser chamado
     # a função dele é criar uma nova conta, então ele recebe como parâmetro um objeto da classe Cliente e um número aleatório
@@ -179,25 +180,45 @@ class Saque(Transacao):
 
 
 class Cliente:
-    def __init__(self, nome, cpf, data_nascimento, endereco):
-        self.nome = nome
-        self.cpf = cpf
-        self.data_nascimento = data_nascimento
-        self.endereco = endereco
+    def __init__(self, endereco, contas):
+        self.endereco = str(endereco)
+        self.contas = list(contas) # cria uma lista vazia para armazenar as contas do cliente. alem de funcionar como um atributo, ele é um parâmetro do construtor
+        self.acao_realizada = False
+        
+    def adicionar_conta(self):
+        conta = Conta.criar_conta(self) # cria uma nova conta para o cliente, essa conta é um objeto da classe Conta
+        self.contas.append(conta) # adiciona a conta à lista de contas do cliente
+        print("Conta adicionada com sucesso")
+        return conta # retorna a conta criada
+        
+        
+    def realizar_transacao(self, conta, transacao):
+            transacao.registrar(conta) # registra a transação na conta, conta é um objeto da classe Conta. transacao é um objeto da classe Transacao, que pode ser Deposito ou Saque, pois é uma abstract class
+            if transacao in conta.historico.transacoes: # 
+                self.acao_realizada = True # na logica do programa, se a transação foi registrada, a ação foi realizada.
+            print("Transação realizada com sucesso")
+
+
+class Pessoa_Fisica(Cliente):
+    def __init__(self, endereco, contas, cpf, nome, data_nascimento):
+        super().__init__(endereco, contas)
+        self.cpf = str(cpf)
+        self.nome = str(nome)
+        self.data_nascimento = datetime.strptime(data_nascimento, "%d/%m/%Y").date() # converte a data de nascimento para o formato date\ 
 
 
 # Criando uma conta
-minha_conta = Conta(100.0, cliente=Cliente(nome="Fulano", cpf="123.456.789-00", data_nascimento="01/01/2000", endereco="Rua A, 123"))
+minha_conta = Conta(100.0, cliente=Cliente(endereco="Rua A, 123", contas=[]))
 
 # Realizando um depósito
 deposito = Deposito(50.0)
 deposito.registrar(minha_conta)
-minha_conta.mostrar_extrato(saldo=minha_conta.saldo, extrato=minha_conta.historico.transacoes)
+minha_conta.mostrar_extrato()
 
 # Realizando um saque
 saque = Saque(30.0)
 saque.registrar(minha_conta)
-minha_conta.mostrar_extrato(saldo=minha_conta.saldo, extrato=minha_conta.historico.transacoes)
+minha_conta.mostrar_extrato()
 
 # Exibindo o histórico de transações
 minha_conta.historico.imprimir()
